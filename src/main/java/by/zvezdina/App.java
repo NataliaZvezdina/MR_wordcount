881c2ffc24184a081b3ce50fad80ec9ddf590ced
natal <natalja.zvezdina@yandex.by>
2022-07-17 12:46:16 +0300

    initialize conf, job, set input, output parameters

--- src/main/java/by/zvezdina/App.java
+++ src/main/java/by/zvezdina/App.java
@@ -1,11 +1,38 @@
 package by.zvezdina;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+
 /**
  * Classic WordCount using Hadoop MapReduce
  *
  */
 public class App {
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws IOException, InterruptedException, ClassNotFoundException {
+        Configuration configuration = new Configuration();
+        Job job = new Job(configuration, "WordCount");
 
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(CustomMapper.class);
+        job.setReducerClass(CustomReducer.class);
+
+        job.waitForCompletion(true);
     }
 }
